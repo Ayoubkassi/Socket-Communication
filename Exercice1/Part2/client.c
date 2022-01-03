ad6d604c0ad4377e7cc6344a6610aa9ff7624356
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-01-04 00:43:29 +0100

    FIX UDP bugs

--- Exercice1/Part2/client.c
+++ Exercice1/Part2/client.c
@@ -1,46 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 #include <netinet/in.h>
 
-#define PORT	 8080
+#define PORT	2021
 #define MAXLINE 1024
 
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
-	char *hello = "Hello from client";
+	char message[200];
+    printf("Entrez un message : ");
+    fgets(message,200,stdin);
+    message[strlen(message)-1] = '\0';
 	struct sockaddr_in	 servaddr;
 
-	// crrer decripteur de fichier socket
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
+		perror("la création du socket a échoué");
 		exit(EXIT_FAILURE);
 	}
 
 	memset(&servaddr, 0, sizeof(servaddr));
 	
-	// completer les informations du serveur
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	
 	int n, len;
 	
-	sendto(sockfd, (const char *)hello, strlen(hello),
+	sendto(sockfd, (const char *)message, strlen(message),
 		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
 			sizeof(servaddr));
-	printf("Hello message sent.\n");
+	printf(" message envoye.\n");
 		
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-				MSG_WAITALL, (struct sockaddr *) &servaddr,
-				&len);
-	buffer[n] = '\0';
-	printf("Server : %s\n", buffer);
 
 	close(sockfd);
 	return 0;
